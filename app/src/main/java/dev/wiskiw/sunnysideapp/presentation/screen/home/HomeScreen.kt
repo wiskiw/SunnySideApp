b6ffbdbad0184157d6d01c6889cc176668bbc11d
Andrei Yablonski <9986008+wiskiw@users.noreply.github.com>
2024-05-03 09:37:25 +0200

    Clean up UI

--- app/src/main/java/dev/wiskiw/sunnysideapp/presentation/screen/home/HomeScreen.kt
+++ app/src/main/java/dev/wiskiw/sunnysideapp/presentation/screen/home/HomeScreen.kt
@@ -6,16 +6,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -27,9 +23,7 @@ import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.MultiplePermissionsState
 import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
-import dev.wiskiw.sunnysideapp.presentation.screen.home.compose.CompositeTemperature
 import dev.wiskiw.sunnysideapp.ui.theme.SunnySideAppTheme
-import dev.wiskiw.sunnysideapp.ui.theme.size
 
 
 @OptIn(ExperimentalPermissionsApi::class)
@@ -75,32 +69,6 @@ private fun Content(
                 modifier = Modifier.align(Alignment.Center),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-
-                val listState = remember {
-                    mutableStateListOf(
-                        true,
-                        true,
-                        false,
-                        false,
-                        true,
-                        false,
-                    )
-                }
-
-                CompositeTemperature(
-                    modifier = Modifier
-                        .padding(MaterialTheme.size.four)
-                        .size(360.dp),
-                    sources = listState,
-                )
-
-                Button(
-                    onClick = {
-                        listState.add(true)
-                    }
-                ) {
-                    Text(text = "Add")
-                }
 
                 if (address != null) {
                     Text(
